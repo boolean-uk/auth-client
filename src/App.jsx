359a2ef1edbf98438e2af71cb4ecd6d335908996
Alexander Lind <alex.anonym@gmail.com>
2022-12-06 13:57:47 +0100

    finished auth-client

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,105 @@
-import './App.css';
-import { useState } from 'react';
-import Form from './components/Form';
-import Input from './components/Input';
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
+import Input from "./components/Input";
 
 export default function App() {
-    const [user, setUser] = useState({ username: '', password: '' });
-    const [registerResponse, setRegisterResponse] = useState('');
-    const [loginResponse, setLoginResponse] = useState('');
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [registerResponse, setRegisterResponse] = useState();
+  const [loginResponse, setLoginResponse] = useState("");
 
-    const register = async (e) => {
-        e.preventDefault();
-        // Write your register code here
+  const register = async (e) => {
+    e.preventDefault();
+    // Write your register code here
+    fetch("http://localhost:4000/register/", {
+      method: "POST",
+      headers: { "Content-type": "application/JSON" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => setRegisterResponse(`User created with username: ${data.user.username}`));
+  };
 
+  const login = async (e) => {
+    e.preventDefault();
+    // Write your login code here
+    fetch("http://localhost:4000/login/", {
+      method: "POST",
+      headers: { "Content-type": "application/JSON" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoginResponse(data.token)
+        localStorage.setItem("token", data.token)
+    });
+  };
 
-    };
+  // You can safely ignore everything below this line, it's just boilerplate
+  // so you can focus on the exercise requirements
 
-    const login = async (e) => {
-        e.preventDefault();
-        // Write your login code here
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-        
-    };
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
 
+  return (
+    <div className="App">
+      <h1>Register</h1>
 
+      <Form
+        handleSubmit={register}
+        inputs={[
+          <Input
+            key={1}
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={user.username}
+            handleChange={handleChange}
+          />,
+          <Input
+            key={2}
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            handleChange={handleChange}
+          />,
+        ]}
+      />
 
+      {registerResponse && <p>{registerResponse}</p>}
 
+      <h1>Login</h1>
 
+      <Form
+        handleSubmit={login}
+        inputs={[
+          <Input
+            key={1}
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={user.username}
+            handleChange={handleChange}
+          />,
+          <Input
+            key={2}
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            handleChange={handleChange}
+          />,
+        ]}
+      />
 
-    // You can safely ignore everything below this line, it's just boilerplate
-    // so you can focus on the exercise requirements
-
-    const handleChange = (e) => {
-        const { value, name } = e.target;
-
-        setUser({
-            ...user,
-            [name]: value
-        });
-    }
-
-    return (
-        <div className="App">
-
-            <h1>Register</h1>
-
-            <Form
-                handleSubmit={register}
-                inputs={[
-                    <Input
-                        key={1}
-                        type='text'
-                        name='username'
-                        placeholder='Username'
-                        value={user.username}
-                        handleChange={handleChange}
-                    />,
-                    <Input
-                        key={2}
-                        type='password'
-                        name='password'
-                        placeholder='Password'
-                        value={user.password}
-                        handleChange={handleChange}
-                    />
-                ]}
-            />
-
-            {registerResponse && <p>{registerResponse}</p>}
-
-            <h1>Login</h1>
-
-            <Form
-                handleSubmit={login}
-                inputs={[
-                    <Input
-                        key={1}
-                        type='text'
-                        name='username'
-                        placeholder='Username'
-                        value={user.username}
-                        handleChange={handleChange}
-                    />,
-                    <Input
-                        key={2}
-                        type='password'
-                        name='password'
-                        placeholder='Password'
-                        value={user.password}
-                        handleChange={handleChange}
-                    />
-                ]}
-            />
-
-            {loginResponse && <p>{loginResponse}</p>}
-
-        </div>
-    );
+      {loginResponse && <p>{loginResponse}</p>}
+    </div>
+  );
 }
