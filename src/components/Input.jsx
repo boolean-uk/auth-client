b3f8a65f132154034d1a5c2eb6ec16e162fa2b3a
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-01-24 16:38:19 +0000

    initial setup

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,17 @@
-export default function Input({ type, value, name, placeholder, handleChange }) {
-    return (
-        <input type={type} placeholder={placeholder} name={name} value={value} onChange={handleChange} />
-    )
+export default function Input({
+	type,
+	value,
+	name,
+	placeholder,
+	handleChange,
+}) {
+	return (
+		<input
+			type={type}
+			placeholder={placeholder}
+			name={name}
+			value={value}
+			onChange={handleChange}
+		/>
+	);
 }
