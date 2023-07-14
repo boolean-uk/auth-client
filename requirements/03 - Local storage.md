bf7a48a5c74e227cb50b9f234763929ad6857527
Chris Sach <christophersach90@gmail.com>
2023-07-14 16:59:04 +0100

    all done

--- requirements/03 - Local storage.md
+++ requirements/03 - Local storage.md
@@ -21,11 +21,13 @@ The important methods to know are:
 
 The user has logged in and now has a valid bearer token saved in local storage. Which header do we need to put this token into when requesting a protected resource from a server?
 
-Answer:
+Answer: Authorisation 
 
 ### Q2
 
 Imagine the below code gets profile information for a user by ID but requires a valid token to access. What would you add to include the token from local storage?
+
+`localStorage.getItem(key)` with the authorisation header
 
 ```js
 fetch('http://localhost:4000/user-profile/1', {
--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,103 @@
-import { useState } from 'react';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
+  const newUser = {
+    username: "",
+    password: "",
+    token: "",
+  };
 
-    const newUser = {
-        username: "",
-        password: ""
+//   Request state
+  const [user, setUser] = useState(newUser);
+
+//   Response state
+  const [saveUser, setSaveUser] = useState(newUser);
+  const [error, setError] = useState("")
+
+  function handleChange(e) {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+
+    if (inputName === "username") {
+      setUser({ ...user, username: inputValue });
+      // console.log(user)
+    } else {
+      setUser({ ...user, password: inputValue });
+      // console.log(user)
     }
+  }
 
-    const [user, setUser] = useState(newUser)
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleChange(e) {
-        const inputName = e.target.name
-        const inputValue = e.target.value
+    console.log(JSON.stringify(user));
+    const inputName = e.target.name;
 
-        if(inputName === "username"){
-            setUser({...user, username: inputValue})
-            // console.log(user)
-        } else {
-            setUser({...user, password: inputValue})
-            // console.log(user)
-
-        }
-
-    }
-
-    function handleSubmit(e) {
-        e.preventdefault()
-        
-        fetch(`http://localhost:4000/npcs`, {
-            method: "POST",
-            body: JSON.stringify(user),
-            headers: { "Content-Type": "application/json" },
+    
+    fetch(`http://localhost:4000/${inputName}`, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: { "Content-Type": "application/json" },
     })
-    }
-    return (
-        <div className="App">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">
-                    Username
-                    <input type="text" name="username" onChange={handleChange}/>
-                </label>
-                <label htmlFor="">
-                    Password
-                    <input type="password" name="password" onChange={handleChange} />
-                </label>
-                <input type='submit' />
-            </form>
+      .then((res) => {
+          res.json()
+          if(res.status === 401){
+        setError('Invalid username or password')
+        } 
+      }
+      )
+      .then((data) => {
+        if(inputName === 'login') {
+        setSaveUser({...saveUser, token: data.token});
+        localStorage.setItem("token", `${saveUser.token}`)
 
+        } else {
+        setSaveUser({...saveUser, username: data.user.username});
+        }
+      })
+
+
+  }
+  return (
+    <>
+      <div className="App">
+        <form>
+          <label htmlFor="">
+            Username
+            <input type="text" name="username" onChange={handleChange} />
+          </label>
+          <label htmlFor="">
+            Password
+            <input type="password" name="password" onChange={handleChange} />
+          </label>
+          <input type="submit" name="register" onClick={handleSubmit} />
+        </form>
+
+        <div>
+          <p>{`Your username is: ${saveUser.username}`}</p>
+          {/* <p>{`Your password is: ${saveUser.password}`}</p> */}
         </div>
-    );
+      </div>
+      <div>
+        <form>
+          <p>LOGIN</p>
+          <label htmlFor="">
+            Username
+            <input type="text" name="username" onChange={handleChange} />
+          </label>
+          <label htmlFor="">
+            Password
+            <input type="password" name="password" onChange={handleChange} />
+          </label>
+          <input type="submit" name="login" onClick={handleSubmit} />
+        </form>
+
+        <div>
+          <p>{`Your token is: ${saveUser.token}`}</p>
+          <p>{`Error?: ${error}`}</p>
+        </div>
+      </div>
+    </>
+  );
 }
