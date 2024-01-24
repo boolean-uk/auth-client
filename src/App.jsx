69f242759d247940a6434acb585db13eeb50816e
ilham saleh <ilham2saleh@gmail.com>
2024-01-24 18:14:25 +0000

    Complete Register and Login functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,74 @@
-import './App.css';
-import { useState } from 'react';
-import Form from './components/Form';
-import Input from './components/Input';
+import "./App.css";
+import { useState } from "react";
+import Form from "./components/Form";
+import Input from "./components/Input";
 
 export default function App() {
-  const [user, setUser] = useState({ username: '', password: '' });
-  const [registerResponse, setRegisterResponse] = useState('');
-  const [loginResponse, setLoginResponse] = useState('');
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [registerResponse, setRegisterResponse] = useState("");
+  const [loginResponse, setLoginResponse] = useState("");
 
   const register = async (e) => {
     e.preventDefault();
     // Write your register code here
+    const { username, password } = user;
 
+    try {
+      const option = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      };
+      const response = await fetch("http://localhost:4000/register", option);
+
+      // fetch("http://localhost:4000/register", option)
+      //   .then((res) => res.json())
+      //   .then((data) => {
+      //     setRegisterResponse(data.user.username);
+      //     console.log(data.user.username);
+      //   });
+
+      if (response.ok) {
+        const result = await response.json();
+        setRegisterResponse(result.user.username);
+      } else {
+        const errorResult = await response.json();
+        setRegisterResponse(errorResult.error);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   const login = async (e) => {
     e.preventDefault();
     // Write your login code here
+    const { username, password } = user;
 
+    try {
+      const option = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      };
+
+      const response = await fetch("http://localhost:4000/login", option);
+
+      if (response.ok) {
+        const result = await response.json();
+        setLoginResponse(result.token);
+        console.log(loginResponse);
+      } else {
+        const errorResult = await response.json();
+        setLoginResponse(errorResult.error);
+      }
+    } catch (error) {
+      setLoginResponse(error.message);
+    }
   };
 
   // You can safely ignore everything below this line, it's just boilerplate
@@ -28,13 +79,12 @@ export default function App() {
 
     setUser({
       ...user,
-      [name]: value
+      [name]: value,
     });
-  }
+  };
 
   return (
     <div className="App">
-
       <h1>Register</h1>
 
       <Form
@@ -42,20 +92,20 @@ export default function App() {
         inputs={[
           <Input
             key={1}
-            type='text'
-            name='username'
-            placeholder='Username'
+            type="text"
+            name="username"
+            placeholder="Username"
             value={user.username}
             handleChange={handleChange}
           />,
           <Input
             key={2}
-            type='password'
-            name='password'
-            placeholder='Password'
+            type="password"
+            name="password"
+            placeholder="Password"
             value={user.password}
             handleChange={handleChange}
-          />
+          />,
         ]}
       />
 
@@ -68,25 +118,24 @@ export default function App() {
         inputs={[
           <Input
             key={1}
-            type='text'
-            name='username'
-            placeholder='Username'
+            type="text"
+            name="username"
+            placeholder="Username"
             value={user.username}
             handleChange={handleChange}
           />,
           <Input
             key={2}
-            type='password'
-            name='password'
-            placeholder='Password'
+            type="password"
+            name="password"
+            placeholder="Password"
             value={user.password}
             handleChange={handleChange}
-          />
+          />,
         ]}
       />
 
       {loginResponse && <p>{loginResponse}</p>}
-
     </div>
   );
 }
