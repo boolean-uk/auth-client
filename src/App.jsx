f728732f516fea8147a1cda4c48f3a9ad910a1d0
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-24 17:13:40 +0000

    feat: register a new user

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from "react";
 import "./App.css";
 import { Form } from "./Form";
 
-const DEFAULT_FORM = {
+const DEFAULT = {
   username: undefined,
   password: undefined,
 };
 
 export default function App() {
-  const [form, setForm] = useState(DEFAULT_FORM);
+
+  const [results, setResults] = useState(DEFAULT);
+
+
   return (
     <div className="App">
-      <Form form={form} setForm={setForm}/>
+      <Form setResult={setResults}/>
       <div>
         <h2>username</h2>
-        <p></p>
+        <p>{results.username}</p>
       </div>
       <div>
         <h2>password</h2>
-        <p></p>
+        <p>{results.password}</p>
       </div>
     </div>
   );
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,36 @@
-function Form({form, setForm}) {
+import { useState } from "react";
 
-    return(<>
-    <form>
+const DEFAULT = {
+    username: undefined,
+    password: undefined,
+  };
+const BASE_URL = "http://localhost:4000"
+
+function Form({ setResults }) {
+    const [form, setForm] = useState(DEFAULT);
+
+
+    function handleSubmission() {
+        
+        const data = {
+            username: form.username,
+            password: form.password
+        }
+
+        const options = {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify(data)
+        }
+        
+        fetch(BASE_URL + "/register", options, data)
+        .then(res => res.json())
+        .then(setResults)
+    }
+
+  return (
+    <>
+      <form onSubmit={handleSubmission}>
         <label>
           username:
           <input
@@ -26,7 +55,8 @@ function Form({form, setForm}) {
         </label>
         <button type="submit">SUBMIT</button>
       </form>
-    </>)
+    </>
+  );
 }
 
-export { Form }
+export { Form };
