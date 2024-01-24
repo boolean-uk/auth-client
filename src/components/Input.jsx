1e6d960310826e59d2332cd97e3760ab44145ff4
Lukas Dembicki <lad_777@hotmail.com>
2024-01-24 18:28:54 +0000

    REFACTOR: spacing.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,11 @@
 export default function Input({ type, value, name, placeholder, handleChange }) {
     return (
-        <input type={type} placeholder={placeholder} name={name} value={value} onChange={handleChange} />
+        <input
+            type={type}
+            placeholder={placeholder}
+            name={name} 
+            value={value}
+            onChange={handleChange}
+        />
     )
 }
