a88d7d7c4e6263af1fd89f9a2df3f72b07951ea0
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-24 15:49:05 +0000

    feat: form html + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css';
 
 export default function App() {
+
+
   return (
     <div className="App">
-
+      <form>
+        <label> username:
+          <input type="text"/>
+        </label>
+        <label> password:
+          <input type="text"/>
+        </label>
+      </form>
+      <div>
+        <h2>username</h2>
+        <p></p>
+      </div>
+      <div>
+        <h2>password</h2>
+        <p></p>
+      </div>
     </div>
   );
 }
