4de3ea994642d74a4ba3268e02b4e38e45d6e40b
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-24 16:52:44 +0000

    feat: update form to be controlled

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const DEFAULT_FORM = {
+  username: undefined,
+  password: undefined,
+};
 
 export default function App() {
-
+  const [form, setForm] = useState(DEFAULT_FORM);
 
   return (
     <div className="App">
       <form>
-        <label> username:
-          <input type="text"/>
+        <label>
+          username:
+          <input
+            name="username"
+            value={form.username}
+            onChange={(e) =>
+              setForm({ ...form, [e.target.name]: e.target.value })
+            }
+            type="text"
+          />
         </label>
-        <label> password:
-          <input type="text"/>
+        <label>
+          password:
+          <input
+            name="password"
+            value={form.password}
+            onChange={(e) =>
+              setForm({ ...form, [e.target.name]: e.target.value })
+            }
+            type="text"
+          />
         </label>
+        <button type="submit">SUBMIT</button>
       </form>
       <div>
         <h2>username</h2>
