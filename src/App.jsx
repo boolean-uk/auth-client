3395a34b55a9bfff3ef03e80c14e3fe4bfca2388
Tayokanch <tayoquadri11@gmail.com>
2024-01-25 13:26:28 +0000

    token saved to the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,56 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './components/Form';
 import Input from './components/Input';
+const url = 'http://localhost:4000';
 
 export default function App() {
   const [user, setUser] = useState({ username: '', password: '' });
   const [registerResponse, setRegisterResponse] = useState('');
   const [loginResponse, setLoginResponse] = useState('');
 
-  const register = async (e) => {
-    e.preventDefault();
-    // Write your register code here
-
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(user),
   };
 
+
+  const register = async (e) => {
+
+    e.preventDefault();
+  
+    try {
+      const response = await fetch(`${url}/register`, options);
+      if (response) {
+        const data = await response.json();
+        setRegisterResponse(data.user.username);
+      } else {
+        setRegisterResponse('Registration failed');
+      }
+    } catch (err) {
+      //console.log('Error during registration', err);
+      return err
+      setRegisterResponse('An error occurred during registration');
+    }
+  };
+  
+
+  
   const login = async (e) => {
     e.preventDefault();
     // Write your login code here
+    try{
+      const verifyLogin = await fetch(`${url}/login`, options);
+        const loginToken = await verifyLogin.json()
+
+        setLoginResponse(loginToken)
+        localStorage.setItem("token", JSON.stringify(loginResponse))
+      
+    }catch(err){
+      console.log('This is the error', err)
+    }
+
 
   };
 
@@ -84,6 +118,7 @@ export default function App() {
           />
         ]}
       />
+
 
       {loginResponse && <p>{loginResponse}</p>}
 
